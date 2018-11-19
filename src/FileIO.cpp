4b2a5145f361962e5f6736d6826e9ea2f5fca860
Francis ANDRE <zosrothko@orange.fr>
2018-11-19 08:52:14 +0100

    Fix compiling with GCC 7.3.0
    Fix 
    ```
    Compiling with GCC 7.3.0 aborted stating the following error:
    
        /qfsm/src/FileIO.cpp:1620:12: error: cannot convert ‘bool’ to ‘Project*’ in return
        return FALSE;
    ```

--- src/FileIO.cpp
+++ src/FileIO.cpp
@@ -1617,7 +1617,7 @@ Project* FileIO::importFile(Import* imp, ScrollView* sv/*=NULL*/)
   QString ext;
 
   if (!imp)
-    return FALSE;
+    return NULL;
 
   Project* p=NULL;
   importdlg->setAcceptMode(QFileDialog::AcceptOpen);
